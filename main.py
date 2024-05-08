2467953f7673fb52f224c3d2bac1add232112a3c
Wellington Juárez <158897351+DannyJPro@users.noreply.github.com>
2024-05-08 09:31:22 -0600

    Update main.py
    aqui nuevamente hago un retoque de la informacion de la universidad rafael landivar, para finalizar este pequeño examen

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 
 print= Wellington Daniel Juarez Velasquez - 1547624
-print= Universidad Rafael Landivar, (502) 2426 2626 
+print= Universidad Rafael Landivar, (502) 2426 2626, ubicado en Quetzaltenango 14ave. 0-43, zona 3, Quetzaltenango.
